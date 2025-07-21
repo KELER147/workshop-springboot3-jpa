7a815fa3828e4ccfc07a50ad2c3a76dd6583b7f3
Leonardo H. Keler <leonardo.keler.14@gmail.com>
2025-07-21 13:21:20 -0300

    Project created

---
+++ src/main/java/com/github/keler147/ws_course_java/WsCourseJavaApplication.java
@@ -0,0 +1,12 @@
+package com.github.keler147.ws_course_java;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsCourseJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsCourseJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/keler147/ws_course_java/WsCourseJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.keler147.ws_course_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsCourseJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
